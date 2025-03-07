70d71110cd255250d222d661b293d333b3edf01c
limonova <limonlnz@icloud.com>
2025-03-07 12:42:02 +0300

    Добавила AlertModel и QuestionFactoryProtocol

---
+++ MovieQuiz/Models/AlertModel.swift
@@ -0,0 +1,15 @@
+///Users/natalia.limonova/Desktop/Projects/MovieQuiz-ios/MovieQuiz/Models/AlertPresenter.swift
+//  AlertPresenter.swift
+//  MovieQuiz
+//
+//  Created by natalia.limonova on 29.12.2024.
+//
+
+import Foundation
+
+struct AlertModel {
+    let title: String
+    let message: String
+    let buttonText: String
+    let completion: (() -> Void)?
+}
